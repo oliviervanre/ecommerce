4f64d993b7b3f76efe1ec7e62f420fca29c3a680
Olivier_V <olivier.vanre@gmail.com>
2024-01-09 11:21:03 +0100

    formulaire ajout produit

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -3,9 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use App\Form\ProductsFormType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/produits', name: 'admin_products_')]
 class ProductsController extends AbstractController
@@ -18,10 +23,46 @@ class ProductsController extends AbstractController
     }
     #[Route('/ajout', name: 'add')]
 
-    public function add() : Response 
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger) : Response 
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/products/index.html.twig');    
+
+        //On crée un nouveau produit
+        $product = new Products();
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        // On traite la requête du formulaire
+        $productForm->handleRequest($request);
+        // On vérifie si le formulaire est soumis ET valide
+        if($productForm->isSubmitted() && $productForm->isValid()){
+            // On génère le Slug
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+
+            // On arrondit le prix 
+            $prix = $product->getPrice() * 100;
+            $product->setPrice($prix);
+
+            // On stocke le produit
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit ajouté avec succès');
+
+            // On redirige
+            //return $this->redirectToRoute('admin_products_index');
+            return $this->redirectToRoute('admin_products_index');
+        }
+
+
+        return $this->render('admin/products/add.html.twig',[
+            'productForm' => $productForm->createView()
+        ]);    
+
+        //   return $this->render('admin/products/index.html.twig', compact('productForm'));
+
+        // compact équivaut à ['productForm' => $productForm]  
+        // mais ce return ne fonctionnement pas avec render et renderForm est deprecated
     }
     #[Route('/edition/{id}', name: 'edit')]
 
